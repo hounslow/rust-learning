db768a3549cca74d8aab273bc253be37945c98da
hounslow <hounslowmatthew@gmail.com>
2018-04-01 09:50:33 -0700

    Adding documentation

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# rust-learning
-A repo to store all the projects I'll do related to learning Rust. 
+# Rust Learning
+
+## Cargo
+- Package manager similar to NPM
+- Can `build` your code, aka compile your code in `src/` into a `target/` folder
+-
+
+## Variables
+- Immutable by default (can be made mutable by using `let mut x = 5`)
+- Constants are always immutable, and must be annotated (e.g `const Y: u32 = 5`)
+- You can 'shadow' variables
+    ```fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+
+    println!("The value of x is: {}", x);
+}```
--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,20 @@
+// fn main() {
+//     let mut x = 5;
+//     // constants are ALWAYS immutable
+//     // values also must be annotated
+//     // must be set to a constant expression
+//     // can't be set to the result of a function call
+//     const Y: u32 = 6;
+//     println!("{}", Y);
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
 fn main() {
     let mut x = 5;
-    // constants are ALWAYS immutable
-    const Y: u32 = 6;
-    println!("{}", Y);
-    println!("The value of x is: {}", x);
-    x = 6;
+    x = x + 1;
+    x = x * 2;
+
     println!("The value of x is: {}", x);
 }
